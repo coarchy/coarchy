bd7c253974445b7c11fce1a43f8dc75cf10948b3
user <acetousk@users.noreply.github.com>
2023-08-14 16:32:08 -0600

    Retry quasar in js

---
+++ screen/coarchy-com/mstatic/WebrootVue.qvt.js
@@ -0,0 +1,20 @@
+/* This software is in the public domain under CC0 1.0 Universal plus a Grant of Patent License. */
+
+moqui.webrootVue = new Vue({
+    el: '#apps-root',
+    data: { basePath:"", linkBasePath:"", currentPathList:[], extraPathList:[], currentParameters:{}, bodyParameters:null,
+    methods: {
+
+    },
+    watch: {
+    },
+    computed: {
+    },
+    created: function() {
+    },
+    mounted: function() {
+    },
+    beforeDestroy: function() {
+    }
+
+});
