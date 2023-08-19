11f3735d32d565058fd177ff223c6f5a8db144da
user <acetousk@users.noreply.github.com>
2023-08-18 22:49:46 -0600

    Create Onboarding, fix signUp, add authz data for coarchy_com

--- screen/coarchy-com/cstatic/CoarchyComVue.qvt.js
+++ screen/coarchy-com/cstatic/CoarchyComVue.qvt.js
@@ -74,11 +74,10 @@ Vue.component('c-sign-up', {
     template:
     // Could include type="email" for email, but then wouldn't allow john.doe to login
         '<q-form @submit="onSubmit" class="q-gutter-md" >\n' +
-        '    <q-input filled v-model="organizationName" label="Organization Name" :rules="[ val => val && val.length > 0 || \'Please type something\' ]"/>\n' +
         '    <q-input filled v-model="emailAddress" type="email" label="Work Email" :rules="[ val => val && val.length > 0 || \'Please type something\' ]"/>\n' +
         '    <q-input filled v-model="firstName" label="First Name" :rules="[ val => val && val.length > 0 || \'Please type something\']"/>\n' +
         '    <q-input filled v-model="lastName" label="Last Name" :rules="[ val => val && val.length > 0 || \'Please type something\']"/>\n' +
-        '    <q-input v-model="newPassword" filled label="New Password" :type="isPwd ? \'password\' : \'text\'" :rules="[ val => val.length >= 8 || \'Please use minimum of 8 characters\', val => /\\d/.test(val) || \'Please use at least 1 number\', val => /[^0-9a-zA-Z]/.test(val) || \'Please use at least 1 special character\' ]">\n' +
+        '    <q-input v-model="newPassword" filled label="New Password" :type="isPwd ? \'password\' : \'text\'" :rules="[ val => val && val.length > 0 || \'Please type something\', val => val.length >= 8 || \'Please use minimum of 8 characters\', val => /\\d/.test(val) || \'Please use at least 1 number\', val => /[^0-9a-zA-Z]/.test(val) || \'Please use at least 1 special character\' ]">\n' +
         '        <template v-slot:append>\n' +
         '            <q-icon :name="isPwd ? \'visibility_off\' : \'visibility\'" class="cursor-pointer" @click="isPwd = !isPwd"/>\n' +
         '        </template>\n' +
@@ -118,7 +117,7 @@ Vue.component('c-sign-up', {
         onSubmit () {
             this.doTheDataThing('/c/SignUp/createAccount', new URLSearchParams(this._data).toString())
                 .then((data) => {
-                    // console.log(data);
+                    console.log(data);
 
                     // Show the errors from the server
                     if (data.errors != null && data.errors.length > 0) {
@@ -164,6 +163,83 @@ Vue.component('c-sign-up', {
 
         },
 
+    }
+});
+Vue.component('c-onboarding', {
+    name: "cOnboarding",
+    template:
+    // Could include type="email" for email, but then wouldn't allow john.doe to login
+        '<q-stepper v-model="step" header-nav ref="stepper" color="primary" animated>\n' +
+        '    <q-step :name="1" title="Create your Organization" icon="settings" :done="done1">\n' +
+        '         <div class="test-body1 q-pb-md">Let\'s create the Organization that you will design.</div>\n' +
+        '         <q-form @submit="createOrganization">\n' +
+        '             <q-input filled v-model="organizationName" label="Organization Name" :rules="[ val => val && val.length > 0 || \'Please type something\' ]"/>\n' +
+        '             <div>\n' +
+        '                 <q-stepper-navigation><q-btn type="submit" color="primary" label="Create and Continue" /></q-stepper-navigation>\n' +
+        '             </div>\n' +
+        '         </q-form>' +
+        '    </q-step>\n' +
+        '    <q-step :name="2" title="Create an ad group" caption="Optional" icon="create_new_folder" :done="done2">An ad group contains one or more ads which target a shared set of keywords.\n' +
+        '        <q-stepper-navigation><q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="Continue" /><q-btn flat @click="step = 1" color="primary" label="Back" class="q-ml-sm" /></q-stepper-navigation>\n' +
+        '    </q-step>\n' +
+        '    <q-step :name="3" title="Create an ad" icon="add_comment" :done="done3">\n' +
+        '        Try out different ad text to see what brings in the most customers, and learn how to enhance your ads using features like ad extensions. If you run into any problems with your ads, find out how to tell if they\'re running and how to resolve approval issues.\n' +
+        '        <q-stepper-navigation>\n' +
+        '            <q-btn color="primary" @click="done3 = true" label="Finish" />\n' +
+        '            <q-btn flat @click="step = 2" color="primary" label="Back" class="q-ml-sm" />\n' +
+        '        </q-stepper-navigation>\n' +
+        '    </q-step>\n' +
+        '</q-stepper>\n',
+    data () {
+        return {
+            organizationName: null,
+            step: 1,
+            done1: false,
+            done2: false,
+            done3: false
+        }
+    },
+    methods: {
+        async doTheDataThing(url = "", data = URLSearchParams) {
+            // See https://web.dev/introduction-to-fetch/#post-request for the fetch api
+            const response = await fetch(url, {
+                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: "same-origin", // include, *same-origin, omit
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: data // See: https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+            })
+
+            return response.json();
+        },
+        createOrganization () {
+            this.doTheDataThing('/c/Onboarding/createOrganization', new URLSearchParams(this._data).toString())
+                .then((data) => {
+                    console.log(data);
+
+                    // Show the errors from the server
+                    if (data.errors != null && data.errors.length > 0) {
+                        const errorList = data.errors.split('/n')
+                        for (const error of errorList) {
+                            // console.log(error)
+                            this.$q.notify({
+                                color: 'red-5',
+                                textColor: 'white',
+                                icon: 'warning',
+                                message: error
+                            })
+                        }
+                    }
+
+                    this.done1 = true; this.step = 2;
+                })
+
+        },
     }
 });
 
