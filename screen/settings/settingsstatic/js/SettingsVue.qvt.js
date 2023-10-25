5c6f2e1d1bb29dc5914fc24d1c4339cd3c12108d
acetousk <acetousk@users.noreply.github.com>
2023-10-24 22:00:14 -0600

    Update localhost

--- screen/settings/settingsstatic/js/SettingsVue.qvt.js
+++ screen/settings/settingsstatic/js/SettingsVue.qvt.js
@@ -69,7 +69,7 @@ Vue.component('m-stripe', {
                 elements,
                 confirmParams: {
                     // Make sure to change this to your payment completion page
-                    return_url: "http://localhost:8080/settings/OrderHistory",
+                    return_url: "https://coarchy.com/settings/OrderHistory",
                     receipt_email: this.emailAddress,
                 },
             });
