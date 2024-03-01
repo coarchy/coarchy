eb7db075490eeaef7abd53f97ca6e05f62b2ae97
Marwan Dessouki <marwan.dessouki@outlook.com>
2024-03-02 00:54:51 +0200

    Add new qvue process screen & components

---
+++ screen/coarchy/costatic/js/ProcessStoryVue.js
@@ -0,0 +1,239 @@
+Vue.component('c-edit-activity', {
+    name: "editActivity",
+    template:
+    '<q-form ref="editActivityForm"\n'+
+        '@submit.prevent="onSubmit">\n'+
+        '<div class="row items-center">\n'+
+        '    <div class="col-3 q-px-xs">\n'+
+        '        <q-input ref="editCondInput" autogrow v-model="conditionVal" dense\n'+
+        '            label="Condition" @keydown.prevent.enter="$refs.editActorSelect.focus()"></q-input>\n'+
+        // '            label="Condition" @keydown.prevent.enter="(e)=>$refs.editActivityForm.submit(e)"></q-input>\n'+
+        '    </div>\n'+
+        '    <div class="col-3 q-px-xs">\n'+
+        '        <q-select ref="editActorSelect" v-model="actorsVal" multiple dense use-input\n'+
+        '            use-chips options-dense :options="actorList" @filter="actorFilterFn"\n'+
+        '            label="Actors"></q-select>\n'+
+        '    </div>\n'+
+        '    <div class="col-6 q-px-xs">\n'+
+        '        <q-input ref="editActionInput" autogrow v-model="actionVal" dense\n'+
+        '            label="Action"\n'+
+        '            @keydown.enter.prevent="(e) => ($refs.editActivityForm.submit(e))" @keydown.tab.prevent="(e) => ($refs.editActivityForm.submit(e))"></q-input>\n'+
+        '    </div>\n'+
+        '</div>\n'+
+    '</q-form>',        
+    props:['condition','actors','action'],
+    emits:['save'],
+    data: function() { 
+        return { 
+            conditionVal: this.condition,
+            actorsVal: this.actors,
+            actionVal: this.action,
+            actorList: [],
+        } 
+    },
+    computed:{
+        dirty(){
+            return (this.conditionVal != this.condition) || !this.areArraysEqualSets(this.actorsVal.map(it=>it.value), this.actors.map(it=>it.value)) || (this.actionVal != this.action)
+        }
+    },  
+    methods: {
+        onSubmit(){
+            if (this.dirty){
+                this.$emit('save', {condition:this.conditionVal,actors:this.actorsVal,action:this.actionVal})
+            }else{
+                this.$emit('close')
+            }
+        },
+        getActorList(term) {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStory/getActorList',
+                data: {
+                    term: term,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },
+        actorFilterFn(val, update, abort) {
+            let vm = this;
+            vm.getActorList(val.toLowerCase()).then(function (resp) {
+                update(() => {
+                    vm.actorList = resp;
+                });
+            });
+        },
+        areArraysEqualSets(a1, a2) {
+            const superSet = {};
+            for (const i of a1) {
+                const e = i + typeof i;
+                superSet[e] = 1;
+            }
+
+            for (const i of a2) {
+                const e = i + typeof i;
+                if (!superSet[e]) {
+                    return false;
+                }
+                superSet[e] = 2;
+            }
+
+            for (let e in superSet) {
+                if (superSet[e] === 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        },
+    },
+    mounted(){
+        this.$refs.editCondInput.focus();
+    }
+});
+
+
+Vue.component('c-undelete-activity-dialog', {
+    name: "undeleteActivityDialog",
+    template:
+    '<q-dialog v-bind="$attrs" v-on="$listeners" @show="getDeletedActivities">\n'+
+    '    <q-card style="min-width:400px; min-height:400px" class="q-pa-md">\n'+
+    '        <q-card-section class="row items-center q-pb-none">\n'+
+    '            <div class="text-h4">Undelete activities</div>\n'+
+    '            <q-space></q-space>\n'+
+    '            <q-btn icon="close" flat round dense v-close-popup></q-btn>\n'+
+    '        </q-card-section>\n'+
+    '        <q-card-section class="q-py-md">\n'+
+    '            <template v-if="!deletedActivityList?.length">\n'+
+    '                <div class="q-pa-lg text-center q-my-md text-grey-8">\n'+
+'                        We couldn\'t find any deleted activities in this process story...\n'+
+    '                </div>\n'+
+    '            </template>\n'+
+    '            <template v-else>\n'+
+    '            <div class="q-my-sm text-caption text-grey-8">\n'+
+    '                Click on a deleted activity to restore it.\n'+
+    '            </div>\n'+
+    '            <q-list bordered separator dense>\n'+
+    '                <q-item clickable v-ripple v-for="deletedActivity in deletedActivityList"\n'+
+    '                    :key="deletedActivity.activityId"\n'+
+    '                    @click="()=>restoreActivity(deletedActivity.processStoryActivityId)"\n'+
+    '                    v-close-popup>\n'+
+    '                    <q-item-section>\n'+
+    '                        <div>\n'+
+    '                            <span class="text-italic">\n'+
+    '                                {{ formatCondition(deletedActivity.condition) }}\n'+
+    '                            </span>\n'+
+    '                            <span class="text-bold">\n'+
+    '                                {{ formatActorNames(deletedActivity.actorNames) }}\n'+
+    '                            </span>\n'+
+    '                            <span class="">\n'+
+    '                                {{ formatAction(deletedActivity.action) }}\n'+
+    '                            </span>\n'+
+    '                        </div>\n'+
+    '                    </q-item-section>\n'+
+    '                </q-item>\n'+
+    '            </q-list>\n'+
+    '            </template>\n'+
+    '        </q-card-section>\n'+
+    '        <q-inner-loading :showing="loading"></q-inner-loading>\n'+
+    '    </q-card>\n'+
+    '</q-dialog>\n',
+    props:['processStoryId'],
+    emits:['refetch'],
+    data: function() { 
+        return { 
+            loading: false,
+            deletedActivityList: [],
+        } 
+    },
+    methods: {
+        // onSubmit(){
+        //     if (this.dirty){
+        //         this.$emit('save', {condition:this.conditionVal,actors:this.actorsVal,action:this.actionVal})
+        //     }else{
+        //         this.$emit('close')
+        //     }
+        // },
+        getDeletedActivities() {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStory/getDeletedActivities',
+                data: {
+                    processStoryId: vm.processStoryId,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    vm.deletedActivityList = responseObj.processStoryActivityList;
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },  
+        restoreActivity(processStoryActivityId) {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStory/restoreActivity',
+                data: {
+                    processStoryActivityId: processStoryActivityId,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    if (responseObj.success){
+                        vm.$emit('refetch')
+                        vm.getDeletedActivities()
+                    }
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },    
+        ensureEndsWith(text, suffix) {
+            return text?.endsWith(suffix) ? text : text + suffix
+        },
+        formatCondition(condition) {
+            return condition ? this.ensureEndsWith(this.toSentenceCase(condition), ",") : ''
+        },
+        formatActorNames(actorNames) {
+            return actorNames?.join(", ") || ''
+        },
+        formatAction(action) {
+            return action ? this.ensureEndsWith(action, '.') : ''
+        },
+        toSentenceCase(str) {
+            return str.replace(/\.\s+([a-z])[^\.]|^(\s*[a-z])[^\.]/g, s => s.replace(/([a-z])/, s => s.toUpperCase()))
+        },
+    },
+    mounted(){
+        this.getDeletedActivities();
+    }
+});
