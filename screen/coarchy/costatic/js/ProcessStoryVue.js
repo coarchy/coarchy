86b64df0323af7555682bfde9175cdefafc946fd
Marwan Dessouki <marwan.dessouki@outlook.com>
2024-03-04 19:32:32 +0200

    Attach statement dialog, edit process story name, add actor, new checklist, go to substory, go to parent story, hide/show process story

--- screen/coarchy/costatic/js/ProcessStoryVue.js
+++ screen/coarchy/costatic/js/ProcessStoryVue.js
@@ -108,8 +108,8 @@ Vue.component('c-edit-activity', {
 Vue.component('c-undelete-activity-dialog', {
     name: "undeleteActivityDialog",
     template:
-    '<q-dialog v-bind="$attrs" v-on="$listeners" @show="getDeletedActivities">\n'+
-    '    <q-card style="min-width:400px; min-height:400px" class="q-pa-md">\n'+
+    '<q-dialog v-bind="$attrs" v-on="$listeners" @show="getDeletedActivities" >\n'+
+    '    <q-card :style="{width:((width||760)+\'px\'),\'max-width\':($q.platform.is.mobile?\'100vw\':\'90vw\')}" class="q-pa-md">\n'+
     '        <q-card-section class="row items-center q-pb-none">\n'+
     '            <div class="text-h4">Undelete activities</div>\n'+
     '            <q-space></q-space>\n'+
@@ -150,7 +150,7 @@ Vue.component('c-undelete-activity-dialog', {
     '        <q-inner-loading :showing="loading"></q-inner-loading>\n'+
     '    </q-card>\n'+
     '</q-dialog>\n',
-    props:['processStoryId'],
+    props:['processStoryId','width'],
     emits:['refetch'],
     data: function() { 
         return { 
@@ -159,13 +159,6 @@ Vue.component('c-undelete-activity-dialog', {
         } 
     },
     methods: {
-        // onSubmit(){
-        //     if (this.dirty){
-        //         this.$emit('save', {condition:this.conditionVal,actors:this.actorsVal,action:this.actionVal})
-        //     }else{
-        //         this.$emit('close')
-        //     }
-        // },
         getDeletedActivities() {
             const vm = this
             this.loading = true
@@ -235,5 +228,202 @@ Vue.component('c-undelete-activity-dialog', {
     },
     mounted(){
         this.getDeletedActivities();
+    }
+});
+
+
+Vue.component('c-attach-statement-dialog', {
+    name: "attachStatementDialog",
+    template:
+    '<q-dialog v-bind="$attrs" v-on="$listeners" @show="getActivityStatementList" >\n'+
+    '    <q-card :style="{width:((width||760)+\'px\'),\'max-width\':($q.platform.is.mobile?\'100vw\':\'90vw\')}" class="q-pa-md">\n'+
+    '        <q-card-section class="row items-center q-pb-none">\n'+
+    '            <div class="text-h4">Connect Statements</div>\n'+
+    '            <q-space></q-space>\n'+
+    '            <q-btn icon="close" flat round dense v-close-popup></q-btn>\n'+
+    '        </q-card-section>\n'+
+    '        <q-card-section class="q-py-md">\n'+
+    '           <div class="row">\n'+
+    '               <div class="col-10 q-px-xs">\n'+
+    '                   <q-select ref="editSelectedStatements" v-model="selectedStatements" multiple dense use-input\n'+
+    '                       use-chips options-dense :options="statementList" @filter="statementFilterFn"\n'+
+    '                       label="Connect Statements"></q-select>\n'+
+    '               </div>\n'+
+    '               <div class="col-2 text-right">\n'+
+    '               <q-btn flat dense label="Add" :disable="!selectedStatements" @click="connectStatements"></q-btn>\n'+
+    '               </div>\n'+
+    '           </div>\n'+
+    '           <div class="q-mt-xl" v-if="activityStatementList?.length">\n'+
+    '               <table>\n'+
+    '                   <tr>\n'+
+    '                       <th align="left">\n'+
+    '                           Connected Statements\n'+
+    '                       </th>\n'+
+    '                       <th align="right">\n'+
+    '                           <q-btn v-if="activityStatementList?.length" flat dense label="Unconnect All" color="negative" @click="()=>unconnectAllStatements()"></q-btn>\n'+
+    '                       </th>\n'+
+    '                   </tr>\n'+
+    '                   <tr v-for="statement in activityStatementList" :key="statement?.valueStatementId">\n'+
+    '                       <td>\n'+
+    '                           {{statement?.valueFull}}\n'+
+    '                       </td>\n'+
+    '                       <td>\n'+
+    '                           <q-btn flat round dense icon="delete" color="negative" @click="()=>unconnectStatement(statement.valueStatementActivityId)">\n'+
+    '                           <q-tooltip>Unconnect statement</q-tooltip>\n'+
+    '                           </q-btn>\n'+
+    '                       </td>\n'+
+    '                   </tr>\n'+
+    '               </table>\n'+
+    '           </div>\n'+
+    '        </q-card-section>\n'+
+    '        <q-inner-loading :showing="loading"></q-inner-loading>\n'+
+    '    </q-card>\n'+
+    '</q-dialog>\n',
+    props:['activityId','width'],
+    emits:['refetch'],
+    data: function() { 
+        return { 
+            selectedStatements:[],
+            loading: false,
+            statementList: [],
+            activityStatementList: [],
+        } 
+    },
+    methods: {
+        getValueList(term) {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStoryNew/getValueList',
+                data: {
+                    term: term,
+                    excludeValueIds: vm.activityStatementList?.map(it=>it.valueStatementId)?.join(',')
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },
+        statementFilterFn(val, update, abort) {
+            let vm = this;
+            vm.getValueList(val.toLowerCase()).then(function (resp) {
+                update(() => {
+                    vm.statementList = resp;
+                });
+            });
+        },
+        getActivityStatementList() {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStoryNew/getActivityStatementList',
+                data: {
+                    activityId: vm.activityId,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    vm.activityStatementList = responseObj || [];
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },  
+        unconnectStatement(valueStatementActivityId) {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStoryNew/deleteValueStatementActivity',
+                data: {
+                    activityId: vm.activityId,
+                    valueStatementActivityId: valueStatementActivityId,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    vm.getActivityStatementList()
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        }, 
+        connectStatements() {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStoryNew/createActivityValue',
+                data: {
+                    activityId: vm.activityId,
+                    valueIdList: vm.selectedStatements.map(it=>it.value),
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    vm.selectedStatements=[]
+                    vm.statementList=[]
+                    vm.getActivityStatementList()
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        }, 
+        unconnectAllStatements() {
+            const vm = this
+            this.loading = true
+            return $.ajax({
+                type: 'POST',
+                url: '/cointernal/Process/EditProcessStoryNew/deleteAllValueStatementActivities',
+                data: {
+                    activityId: vm.activityId,
+                },
+                dataType: 'json',
+                headers: {
+                    Accept: 'application/json',
+                    'X-CSRF-Token': this.$root.moquiSessionToken,
+                },
+                error: moqui.handleAjaxError,
+                success: function (responseObj, status, jqXHR) {
+                    vm.getActivityStatementList()
+                    return responseObj
+                },
+                complete: function () {
+                    vm.loading = false
+                },
+            })
+        },           
+    },
+    mounted(){
+        this.getActivityStatementList();
     }
 });
