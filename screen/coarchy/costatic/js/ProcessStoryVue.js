f00582db84594cbdb21424c305c5f62ef91beae0
acetousk <acetousk@users.noreply.github.com>
2024-03-20 23:46:32 -0600

    Still can't not submit on select (broken)

--- screen/coarchy/costatic/js/ProcessStoryVue.js
+++ screen/coarchy/costatic/js/ProcessStoryVue.js
@@ -1,48 +1,67 @@
 Vue.component('c-edit-activity', {
     name: "editActivity",
     template:
-    '<q-form ref="editActivityForm"\n'+
-        '@submit.prevent="onSubmit">\n'+
-        '<div class="row items-center">\n'+
-        '    <div class="col-3 q-px-xs">\n'+
-        '        <q-input ref="editCondInput" autogrow v-model="updateActivityFields.condition" dense\n'+
-        '            label="Condition" @keydown.prevent.enter="$refs.editActorSelect.focus()"></q-input>\n'+
-        // '            label="Condition" @keydown.prevent.enter="(e)=>$refs.editActivityForm.submit(e)"></q-input>\n'+
-        '    </div>\n'+
-        '    <div class="col-3 q-px-xs">\n'+
-        '        <q-select ref="editActorSelect" v-model="updateActivityFields.actors" multiple dense use-input\n'+
-        '            use-chips options-dense :options="actorList" @filter="actorFilterFn"\n'+
-        '            label="Actors"></q-select>\n'+
-        '    </div>\n'+
-        '    <div class="col-6 q-px-xs">\n'+
-        '        <q-input ref="editActionInput" autogrow v-model="updateActivityFields.action" dense\n'+
-        '            label="Action"\n'+
-        '            @keydown.enter.prevent="(e) => ($refs.editActivityForm.submit(e))" @keydown.tab.prevent="(e) => ($refs.editActivityForm.submit(e))"></q-input>\n'+
-        '    </div>\n'+
-        '</div>\n'+
-    '</q-form>',
+    '<q-form ref="editActivityForm" @submit.prevent="createActivity" >\n' +
+        '                    <div class="row">\n' +
+        '                        <div class="col-3 q-px-xs">\n' +
+        '                            <q-input ref="editCondInput" autogrow v-model="conditionVal" dense\n' +
+        '                                label="Condition" @keydown.enter="$refs.selectActorInput.focus()"></q-input>\n' +
+        '                        </div>\n' +
+        '                        <div class="col-3 q-px-xs">\n' +
+        '                            <q-select ref="editActorSelect" v-model="actorsVal" multiple dense\n' +
+        '                                 use-chips options-dense :options="actorList" @filter="actorFilterFn"\n' +
+        '                                label="Actors" @save.prevent="createActivity"></q-select>\n' +
+        '                        </div>\n' +
+        '                        <div class="col-6 q-px-xs">\n' +
+        '                            <q-input ref="editActionInput" autogrow v-model="actionVal" dense\n' +
+        '                                label="Action"\n' +
+        '                                @keydown.enter.prevent="(e) => ($refs.createActivityForm.submit(e))"></q-input>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                </q-form>'
+    //     '<q-form ref="editActivityForm"\n'+
+    //     '@submit.prevent="onSubmit">\n'+
+    //     '<div class="row items-center">\n'+
+    //     '    <div class="col-3 q-px-xs">\n'+
+    //     '        <q-input ref="editCondInput" autogrow v-model="updateActivityFields.condition" dense\n'+
+    //     '            label="Condition" @keydown.prevent.enter="$refs.selectActorInput.focus()"></q-input>\n'+
+    //     // '            label="Condition" @keydown.prevent.enter="(e)=>$refs.editActivityForm.submit(e)"></q-input>\n'+
+    //     '    </div>\n'+
+    //     '    <div class="col-3 q-px-xs">\n'+
+    //     '                            <q-select ref="editActorSelect" v-model="updateActivityFields.actors" multiple dense\n' +
+    //     '                                use-input use-chips options-dense :options="actorList" @filter="actorFilterFn"\n' +
+    //     '                                label="Actors"></q-select>'+
+    //     '    </div>\n'+
+    //     '    <div class="col-6 q-px-xs">\n'+
+    //     '        <q-input ref="editActionInput" autogrow v-model="updateActivityFields.action" dense\n'+
+    //     '            label="Action"\n'+
+    //     '            @keydown.enter.prevent="(e) => ($refs.editActivityForm.submit(e))" @keydown.tab.prevent="(e) => ($refs.editActivityForm.submit(e))"></q-input>\n'+
+    //     '    </div>\n'+
+    //     '</div>\n'+
+    // '</q-form>'
+    ,
     props:['condition','actors','action'],
-    emits:['save'],
-    data: function() { 
+    // emits:['save'],
+    data: function() {
         return {
-            updateActivityFields: {
-                condition: this.condition,
-                actors: this.actors,
-                action: this.action,
-            },
+            // updateActivityFields: {
+                conditionVal: this.condition,
+                actorsVal: this.actors,
+                actionVal: this.action,
+            // },
             actorList: [],
         } 
     },
     computed:{
         dirty(){
-            return (this.updateActivityFields.condition != this.condition) || !this.areArraysEqualSets(this.updateActivityFields.actors.map(it=>it.value), this.actors.map(it=>it.value)) || (this.updateActivityFields.action != this.action)
+            return (this.conditionVal != this.condition) || !this.areArraysEqualSets(this.actorsVal.map(it=>it.value), this.actors.map(it=>it.value)) || (this.actionVal != this.action)
         }
     },  
     methods: {
         onSubmit(){
             if (this.dirty){
-                console.log('dirty', this.updateActivityFields)
-                this.$emit('save', {condition:this.updateActivityFields.condition,actors:this.updateActivityFields.actors,action:this.updateActivityFields.action})
+                console.log('dirty', this)
+                this.$emit('save', {condition:this.conditionVal,actors:this.actorsVal,action:this.actionVal})
             }else{
                 this.$emit('close')
             }
